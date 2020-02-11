04d6d916c9e778fc56be1c7d2a6e733b1bedd613
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-11 12:37:11 +0000

    Initial project setup

---
+++ auto-layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by Bradley Chesworth on 11/02/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
